191045277b983ea6b0a003825fda1d628f478e49
unknown <caelen.yoong@gmail.com>
2022-05-19 23:55:44 -0700

    DON'T TOUCH MY TIMER CLASS

--- Timer.java
+++ Timer.java
@@ -3,16 +3,21 @@ import java.util.concurrent.TimeUnit;
 
 public class Timer{
     //https://stackoverflow.com/questions/10820033/make-a-simple-timer-in-java
+    //DON'T TOUCH MY TIMER CLASS @EVERYONE
 
     public static void main(String[] args){
         long startTime = System.currentTimeMillis();
+        long endTime = System.currentTimeMillis() + 300;
+        System.out.println(endTime);
+        System.out.println(startTime);
         int x = 0;
-        while(x < 61){
+        while(x < 5){
             try {
                 TimeUnit.SECONDS.sleep(1);
                 x++;
                 long elapsedTime = System.currentTimeMillis() - startTime;
-                long elapsedSeconds = elapsedTime / 1000;
+                long elapsedTime2 = System.currentTimeMillis() - endTime;
+                long elapsedSeconds = elapsedTime2 / 1000;
                 long secondsDisplay = elapsedSeconds % 60;
                 long minutesDisplay = elapsedSeconds / 60;
                 String time = minutesDisplay + ":" + secondsDisplay;
@@ -22,14 +27,12 @@ public class Timer{
                 else{
                     System.out.println(time);
                 }
-
             } 
             catch(InterruptedException e) {
-                System.out.println("Got Interrupted!");
-            
+                System.out.println("Timer Failed");
             
             }
 
-            }
+        }
     }
 }
