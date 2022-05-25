9116986f19f31ec73e0a310a0f962955ea0d560b
nleeks <77749368+nleeks@users.noreply.github.com>
2022-05-25 10:57:35 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -80,10 +80,10 @@ public class Game{
             System.out.println("This will not run");
         }
 
-        boolean yes = false
-        boolean no = false
+        boolean yes = false;
+        boolean no = false;
         if (a || b){
-            System.out.println(something is wrong);
+            System.out.println("something is wrong");
         }
         
     }
